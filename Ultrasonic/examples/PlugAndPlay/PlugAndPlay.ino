163b56364238fe573aa467eb9574d4fecb0bbdcd
J.Rodrigo <javirok@hotmail.com>
2013-01-15 02:05:56 +0100

    New Example 26/11/2012
    PlugAndPlay

---
+++ Ultrasonic/examples/PlugAndPlay/PlugAndPlay.ino
@@ -0,0 +1,24 @@
+// Ultrasonic - Library for HR-SC04 Ultrasonic Ranging Module.
+// Rev.4 (06/2012)
+// J.Rodrigo ( http://www.jra.so )
+// more info at http://www.ardublog.com
+// Wiki: https://github.com/elrodri/Ultrasonic-HC-SR04/wiki/Plug-&-Play
+
+#include <Ultrasonic.h>
+
+Ultrasonic ultrasonic(5,6); // (Trig PIN,Echo PIN)
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(4, OUTPUT); // VCC pin
+  pinMode(7, OUTPUT); // GND ping
+  digitalWrite(4, HIGH); // VCC +5V mode  
+  digitalWrite(7, LOW);  // GND mode
+}
+
+void loop()
+{
+  Serial.print(ultrasonic.Ranging(CM)); // CM or INC
+  Serial.println(" cm" );
+  delay(100);
+}
